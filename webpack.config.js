f03969eb03e744ecfb16dd67e66aa425a873ecce
MARCHAL Simon <marchal.simon@gmail.com>
2021-01-28 22:18:53 +0100

    Relative path for icon fonts

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
                     {
                         loader: MiniCssExtractPlugin.loader,
                         options: {
-                            publicPath: '/'
+                            publicPath: './'
                         }
                     },
                     'css-loader'],
