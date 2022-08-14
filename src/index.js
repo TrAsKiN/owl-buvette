e32631a4af51a1f8e3c7844e1d6447d9b54919dc
TrAsKiN <marchal.simon@gmail.com>
2022-08-15 01:59:19 +0200

    Improve UI and add OWL teams colors

--- src/index.js
+++ src/index.js
@@ -6,37 +6,29 @@ import './style.scss';
 
 Vue.component('player', {
     props: ['url', 'is-active', 'is-pip'],
-    template: `
-        <div
-            v-show="(!isPip || (isPip && isActive))"
-            v-bind:class="{pip: isPip}"
-        >
-            <iframe :src="url"></iframe>
-        </div>
-    `,
-
+    template: `<iframe
+        v-show="(!isPip || (isPip && isActive))"
+        v-bind:class="{pip: isPip}"
+        :src="url"
+    ></iframe>`,
 });
 
 Vue.component('link-cast', {
     props: ['hash', 'name', 'active'],
-    template: `
-      <li><a
-          v-bind:class="active"
-          :href="hash"
-          class="dropdown-item"
-      >{{ name }}</a></li>
-    `
+    template: `<li><a
+        v-bind:class="active"
+        :href="hash"
+        class="dropdown-item"
+    >{{ name }}</a></li>`
 });
 
 Vue.component('theme', {
     props: ['hash', 'name', 'active'],
-    template: `
-      <li><a
-          v-bind:class="active"
-          :href="hash"
-          class="dropdown-item"
-      >{{ name }}</a></li>
-    `
+    template: `<li><a
+        v-bind:class="active"
+        :href="hash"
+        class="dropdown-item"
+    >{{ name }}</a></li>`
 });
 
 new Vue({
@@ -44,13 +36,13 @@ new Vue({
     data: {
         players: [
             {
-                id: 'twitch-embed',
-                isPip: false,
+                id: 'host-embed',
+                isPip: true,
                 url: 'https://player.twitch.tv/?channel=fefegg&parent=traskin.github.io&parent=localhost'
             },
             {
-                id: 'youtube-embed',
-                isPip: true,
+                id: 'cast-embed',
+                isPip: false,
                 url: 'https://www.youtube.com/embed/live_stream?channel=UCI45pR3yMi0uGE47ewT43Ow&autoplay=1'
             }
         ],
@@ -130,7 +122,7 @@ new Vue({
             {
                 hash: '#contendersTrials',
                 name: 'Contenders Trials EU',
-                disabled: false,
+                disabled: true,
                 url: 'https://www.youtube.com/embed/live_stream?channel=UCLj0Gz2FQKDyCWE5gk4iO_A&autoplay=1'
             },
             {
@@ -204,29 +196,147 @@ new Vue({
                 name: 'Team Peps',
                 disabled: false,
             },
+            {
+                hash: '#atl',
+                name: 'Atlanta Reign',
+                disabled: false,
+            },
+            {
+                hash: '#bos',
+                name: 'Boston Uprising',
+                disabled: false,
+            },
+            {
+                hash: '#cdh',
+                name: 'Chengdu Hunters',
+                disabled: false,
+            },
+            {
+                hash: '#dal',
+                name: 'Dallas Fuel',
+                disabled: false,
+            },
+            {
+                hash: '#fla',
+                name: 'Florida Mayhem',
+                disabled: false,
+            },
+            {
+                hash: '#gla',
+                name: 'Los Angeles Gladiators',
+                disabled: false,
+            },
+            {
+                hash: '#gzc',
+                name: 'Guangzhou Charge',
+                disabled: false,
+            },
+            {
+                hash: '#hou',
+                name: 'Houston Outlaws',
+                disabled: false,
+            },
+            {
+                hash: '#hzs',
+                name: 'Hangzhou Spark',
+                disabled: false,
+            },
+            {
+                hash: '#ldn',
+                name: 'London Spitfire',
+                disabled: false,
+            },
+            {
+                hash: '#nye',
+                name: 'New York Excelsior',
+                disabled: false,
+            },
+            {
+                hash: '#par',
+                name: 'Paris Eternal',
+                disabled: false,
+            },
+            {
+                hash: '#phi',
+                name: 'Philadelphia Fusion',
+                disabled: false,
+            },
+            {
+                hash: '#seo',
+                name: 'Seoul Dynasty',
+                disabled: false,
+            },
+            {
+                hash: '#sfs',
+                name: 'San Francisco Shock',
+                disabled: false,
+            },
+            {
+                hash: '#shd',
+                name: 'Shanghai Dragons',
+                disabled: false,
+            },
+            {
+                hash: '#tor',
+                name: 'Toronto Defiant',
+                disabled: false,
+            },
+            {
+                hash: '#val',
+                name: 'Los Angeles Valiant',
+                disabled: false,
+            },
+            {
+                hash: '#van',
+                name: 'Vancouver Titans',
+                disabled: false,
+            },
+            {
+                hash: '#was',
+                name: 'Washington Justice',
+                disabled: false,
+            },
         ],
         flipX: false,
         flipY: false,
         pipActive: true,
-        showChat: true
+        showChat: true,
+        aboveChat: false,
     },
     methods: {
         mirrorPip: function () {
             this.flipX = !this.flipX;
+            localStorage.setItem('flipX', this.flipX);
         },
         movePip: function () {
             this.flipY = !this.flipY;
+            localStorage.setItem('flipY', this.flipY);
         },
         togglePip: function () {
             this.pipActive = !this.pipActive;
+            localStorage.setItem('pipActive', this.pipActive);
         },
         toggleChat: function () {
             this.showChat = !this.showChat;
+            localStorage.setItem('showChat', this.showChat);
+            document.querySelector('html').classList.toggle('fullscreen');
+            if (!this.showChat) {
+                document.querySelector(':root').style.setProperty('--chat-width', '0px');
+            } else {
+                document.querySelector(':root').style.setProperty('--chat-width', '380px');
+            }
+            if (this.aboveChat) {
+                this.moveAbove();
+            }
         },
         switchPlayer: function () {
-            this.players.forEach(function (player, index, array) {
+            this.players.forEach((player, index, array) => {
                 array[index].isPip = !player.isPip;
+                if (player.isPip) {
+                    localStorage.setItem('pip', player.id);
+                }
             });
+            console.log(localStorage.getItem('pip'));
         },
         reloadPlayers: function () {
             document.querySelectorAll('#players iframe').forEach(player => player.src += '');
@@ -235,7 +345,7 @@ new Vue({
             if (document.querySelector('#casts .dropdown-item.active')) {
                 document.querySelector('#casts .dropdown-item.active').classList.remove('active');
             }
-            const owlPlayer = document.querySelector('#youtube-embed iframe');
+            const owlPlayer = document.querySelector('#cast-embed');
             let newCast;
             if (event.target) {
                 newCast = event.target.hash;
@@ -246,7 +356,7 @@ new Vue({
             document.querySelector('#casts .dropdown-item[href="'+ newCast +'"]').classList.add('active');
         },
         reloadChat: function () {
-            document.querySelector('#chat').src += '';
+            document.querySelector('#chat iframe').src += '';
         },
         switchTheme: function (event) {
             if (event.target) {
@@ -262,17 +372,48 @@ new Vue({
                 document.querySelector('#themes .dropdown-item[href="'+ localStorage.getItem('theme') +'"]').classList.add('active');
             }
         },
+        moveAbove: function () {
+            this.aboveChat = !this.aboveChat;
+            localStorage.setItem('aboveChat', this.aboveChat);
+            document.querySelector('html').classList.toggle('above');
+            if (!this.pipActive) {
+                this.togglePip();
+            }
+        },
+        reloadSettings: function () {
+            if (localStorage.getItem('flipX') == 'true') {
+                this.mirrorPip();
+            }
+            if (localStorage.getItem('flipY') == 'true') {
+                this.movePip();
+            }
+            if (localStorage.getItem('pipActive') == 'false') {
+                this.togglePip();
+            }
+            if (localStorage.getItem('showChat') == 'false') {
+                this.toggleChat();
+            }
+            if (localStorage.getItem('aboveChat') == 'true') {
+                this.moveAbove();
+            }
+            if (localStorage.getItem('pip')) {
+                this.players.forEach(player => {
+                    if (player.id == localStorage.getItem('pip') && !player.isPip) {
+                        this.switchPlayer();
+                    }
+                });
+            }
+        },
     },
     mounted: function () {
+        this.reloadSettings();
         if (!localStorage.getItem('theme')) {
             localStorage.setItem('theme', '#dark');
         }
         this.switchTheme(this);
-
         if (window.location.hash) {
             this.changeCast(this);
         }
-        this.switchPlayer();
 
         let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
         tooltipTriggerList.map(function (tooltipTriggerEl) {
@@ -293,34 +434,5 @@ new Vue({
             .then(({ data }) => {
                 document.querySelector('#version').innerHTML = data.tag_name;
             });
-
-        /*
-        let isResizing = false;
-        let x = 0;
-        const resizer = document.querySelector('#resizer');
-        const chat = document.querySelector('.col-chat');
-        const root = document.querySelector(':root');
-        resizer.addEventListener('mousedown', e => {
-            isResizing = true;
-            x = e.x;
-        });
-        window.addEventListener('mousemove', e => {
-            if (isResizing === true) {
-                const position = (chat.clientWidth + (x - e.x)) + 'px';
-                root.style.setProperty('--chat-width', position);
-                x = e.x;
-            }
-        });
-        resizer.addEventListener('mouseleave', () => {
-            if (isResizing === true) {
-                isResizing = false;
-            }
-        });
-        window.addEventListener('mouseup', () => {
-            if (isResizing === true) {
-                isResizing = false;
-            }
-        });
-        */
     }
 });
