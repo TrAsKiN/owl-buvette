39f9580b9d876f466c0887035aac60c3d8f3af5a
TrAsKiN <marchal.simon@gmail.com>
2023-03-25 17:39:42 +0100

    feat: add version checker

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import { Octokit } from '@octokit/rest'
 import { Popover } from 'bootstrap/dist/js/bootstrap'
+import Package from '../package.json'
 
 export class Footer extends Component {
   constructor (props) {
     super(props)
-    this.state = { version: 'v3' }
+    this.state = {
+      version: `v${Package.version}`,
+      upToDate: false
+    }
   }
 
   componentDidMount () {
@@ -15,7 +19,10 @@ export class Footer extends Component {
         repo: 'owl-buvette'
       })
       .then(({ data }) => {
-        this.setState({ version: data.tag_name })
+        if (data.tag_name > this.state.version) {
+          console.warn(`Your app version is not up-to-date!`)
+          this.setState({upToDate: false})
+        }
       })
 
     const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
@@ -34,7 +41,7 @@ export class Footer extends Component {
             <> &middot; </>
             <>Sources on <a href='https://github.com/TrAsKiN/owl-buvette' target='_blank' rel='noreferrer'>GitHub</a></>
             <> &middot; </>
-            <span id='version'>{this.state.version}</span>
+            <span id='version' className='position-relative'>{this.state.upToDate || <i className='bi bi-exclamation text-warning text-opacity-50 position-absolute start-0 translate-middle' style={{top: 30+'%'}}></i>}{this.state.version}</span>
             <> &middot; </>
             <a href='https://github.com/TrAsKiN/owl-buvette/issues/new' target='_blank' rel='noreferrer'>Report a bug</a>
             <> &middot; </>
@@ -42,7 +49,7 @@ export class Footer extends Component {
               tabIndex='0' className='text-decoration-none badge rounded-pill text-bg-info text-dark' role='button'
               data-bs-toggle='popover' data-bs-trigger='focus' data-bs-content='<a href="https://www.youtube.com/account_sharing" target="_blank" class="text-dark text-decoration-none fw-bold">Lié son compte Battle.net à YouTube</a><br><a href="https://account.battle.net/connections#connected-accounts" target="_blank" class="text-dark text-decoration-none fw-bold">Lié son compte Battle.net à Twitch</a>'
             >
-              Drops ?
+              Drops?
             </a>
           </small>
         </p>
