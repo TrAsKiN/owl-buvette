261c906bd62519827f10256855437b7a70f6b33b
TrAsKiN <marchal.simon@gmail.com>
2023-07-02 12:37:21 +0200

    wip

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -2,9 +2,10 @@ import React, { Component } from 'react'
 
 export class Chat extends Component {
   render () {
+    const isLightTheme = this.props.theme === 'light'
     return (
       <div id='chat' className={this.props.showChat ? '' : 'd-none'}>
-        <iframe src={`https://www.twitch.tv/embed/${this.props.host ?? this.props.channel}/chat?darkpopout&parent=${window.location.hostname}`} style={{zIndex: 11000}} />
+        <iframe src={`https://www.twitch.tv/embed/${this.props.host ?? this.props.channel}/chat${isLightTheme ? '?' : '?darkpopout&'}parent=${window.location.hostname}`} />
       </div>
     )
   }
