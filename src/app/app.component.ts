eba57637368099f34d5d54da723aaad95b1ddc2b
TrAsKiN <marchal.simon@gmail.com>
2023-08-27 12:24:32 +0200

    chore: remove unnecessary component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from "@angular/common";
-import { ChangeDetectorRef, Component, Renderer2 } from "@angular/core";
+import { Component, Renderer2 } from "@angular/core";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { ActivatedRoute, RouterOutlet } from "@angular/router";
 import { filter } from "rxjs";
@@ -9,7 +9,6 @@ import { ChatComponent } from "./chat/chat.component";
 import { FooterComponent } from "./footer/footer.component";
 import { Player, PlayerComponent } from "./player/player.component";
 import { PlayersCommandsComponent } from "./players-commands/players-commands.component";
-import { PlayersComponent, Position } from "./players/players.component";
 import { THEMES, Theme } from "./theme";
 
 export type Pip = "host" | "cast";
@@ -21,7 +20,6 @@ export type Pip = "host" | "cast";
   imports: [
     CommonModule,
     RouterOutlet,
-    PlayersComponent,
     PlayerComponent,
     ChatComponent,
     FooterComponent,
@@ -32,66 +30,62 @@ export type Pip = "host" | "cast";
 export class AppComponent {
   private pip: Pip = "host";
 
-  protected theme = window.matchMedia("(prefers-color-scheme: dark)").matches
-    ? "theme-dark"
-    : "theme-light";
+  protected theme?: Theme = THEMES.find(
+    (theme) =>
+      theme.id ===
+      (window.matchMedia("(prefers-color-scheme: dark)") ? "dark" : "light"),
+  );
   protected channel = "fefegg";
-  protected position: Position = {
+  protected position = {
     flipX: false,
     flipY: false,
   };
   protected host: Player = {
     id: "host",
-    isPip: this.pip === "host",
+    isPip: true,
     url: `https://player.twitch.tv/?channel=${this.channel}`,
   };
   protected cast: Player = {
     id: "cast",
-    isPip: this.pip === "cast",
+    isPip: false,
     url: null,
   };
-
   protected selectedCast?: Cast;
-  protected selectedTheme?: Theme = THEMES.find(
-    (theme) =>
-      theme.id ===
-      (window.matchMedia("(prefers-color-scheme: dark)") ? "dark" : "light"),
-  );
 
-  constructor(
-    renderer: Renderer2,
-    route: ActivatedRoute,
-    cd: ChangeDetectorRef,
-  ) {
+  constructor(renderer: Renderer2, route: ActivatedRoute) {
     route.fragment
       .pipe(
-        filter((fragment) => fragment !== ""),
         takeUntilDestroyed(),
+        filter((fragment) => fragment !== ""),
       )
       .subscribe((fragment) => {
-        if (fragment) {
-          const cast = CASTS.find(
-            (cast) => cast.hash.slice(1) === fragment && !cast.disabled,
-          );
-          if (cast) {
-            this.cast.url = cast.url;
-            this.selectedCast = cast;
-            return;
-          }
+        const cast = CASTS.find(
+          (cast) => cast.hash.slice(1) === fragment && !cast.disabled,
+        );
+        if (cast && this.cast.url !== cast.url) {
+          this.cast.url = cast.url;
+          this.selectedCast = cast;
+          return;
         }
         const defaultCast = CASTS.find((cast) => !cast.disabled);
-        if (defaultCast) {
+        if (defaultCast && this.cast.url !== defaultCast.url) {
           this.cast.url = defaultCast.url;
           this.selectedCast = defaultCast;
           return;
         }
         throw new Error("Unable to find a cast!");
       });
-    renderer.setAttribute(
-      window.document.body,
-      "data-bs-theme",
-      window.matchMedia("(prefers-color-scheme: dark)") ? "dark" : "light",
-    );
-    renderer.addClass(window.document.body, this.theme);
+    if (this.theme) {
+      renderer.setAttribute(
+        window.document.body,
+        "data-bs-theme",
+        this.theme?.id,
+      );
+      renderer.addClass(window.document.body, this.theme.id);
+    }
+  }
+
+  protected isDark() {
+    return this.theme?.id === "dark";
   }
 }
